41c36577d06e98174370aa8f7878d7c17071d46a
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2020-10-23 15:40:35 +0600

    Real Estate Price Prediction Project Added

---
+++ Real Estate Price Prediction Project/Real Estate Price Prediction Project_ Web/server/util.py
@@ -0,0 +1,52 @@
+import pickle
+import json
+import numpy as np
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_estimated_price(location,sqft,bhk,bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index>=0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0],2)
+
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global  __data_columns
+    global __locations
+
+    with open("C:\\Users\\Nayeem\\Python & ML\\Self_ML\\Real Estate Price Prediction Project\\Real Estate Price Prediction Project_ Web\\server\\artifacts\columns.json", 'r') as f1:
+        __data_columns = json.load(f1)['data_columns']
+        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+
+    global __model
+    if __model is None:
+        with open("C:\\Users\\Nayeem\\Python & ML\\Self_ML\\Real Estate Price Prediction Project\\Real Estate Price Prediction Project_ Web\\server\\artifacts\\banglore_home_prices_model.pickle", 'rb') as f2:
+            __model = pickle.load(f2)
+    print("loading saved artifacts...done")
+
+def get_location_names():
+    return __locations
+
+def get_data_columns():
+    return __data_columns
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
